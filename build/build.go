a14436599d89bdaf0071435e221713df9bc4294e
Ben Fickes <bfickes@pivotal.io>
2018-10-24 15:25:08 -0400

    Make cache layer directory before copying packag.json into it
    [#161396774]
    
    Co-authored-by: Zander Mackie <amackie@pivotal.io>

--- build/build.go
+++ build/build.go
@@ -9,18 +9,26 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/fatih/color"
-
-	"github.com/cloudfoundry/npm-cnb/detect"
-
-	"github.com/cloudfoundry/npm-cnb/utils"
-
 	libbuildpackV3 "github.com/buildpack/libbuildpack"
 	"github.com/cloudfoundry/libjavabuildpack"
+	"github.com/cloudfoundry/npm-cnb/detect"
+	"github.com/cloudfoundry/npm-cnb/utils"
+	"github.com/fatih/color"
 )
 
+func CreateLaunchMetadata() libbuildpackV3.LaunchMetadata {
+	return libbuildpackV3.LaunchMetadata{
+		Processes: libbuildpackV3.Processes{
+			libbuildpackV3.Process{
+				Type:    "web",
+				Command: "npm start",
+			},
+		},
+	}
+}
+
 type ModuleInstaller interface {
-	Install(string) error
+	Install(string, string) error
 	Rebuild(string) error
 }
 
@@ -44,19 +52,19 @@ func NewModules(builder libjavabuildpack.Build, npm ModuleInstaller) (Modules, b
 	}
 
 	modules := Modules{
-		npm:    npm,
-		app:    builder.Application,
-		logger: builder.Logger,
+		npm:         npm,
+		app:         builder.Application,
+		logger:      builder.Logger,
+		cacheLayer:  builder.Cache.Layer(detect.NPMDependency),
+		launchLayer: builder.Launch.Layer(detect.NPMDependency),
 	}
 
 	if val, ok := bp.Metadata["build"]; ok {
 		modules.buildContribution = val.(bool)
-		modules.cacheLayer = builder.Cache.Layer(detect.NPMDependency)
 	}
 
 	if val, ok := bp.Metadata["launch"]; ok {
 		modules.launchContribution = val.(bool)
-		modules.launchLayer = builder.Launch.Layer(detect.NPMDependency)
 	}
 
 	return modules, true, nil
@@ -72,6 +80,7 @@ func (m Modules) Contribute() error {
 	}
 
 	appModulesDir := filepath.Join(m.app.Root, "node_modules")
+	cacheModulesDir := filepath.Join(m.cacheLayer.Root, "node_modules")
 	launchModulesDir := filepath.Join(m.launchLayer.Root, "node_modules")
 
 	vendored, err := libjavabuildpack.FileExists(appModulesDir)
@@ -84,22 +93,53 @@ func (m Modules) Contribute() error {
 		return fmt.Errorf("failed in checking shas: %v", err)
 	}
 
+	boldNode := color.New(color.FgBlue, color.Bold).Sprint("Node Modules")
 	if !sameSHASums {
-		m.logger.FirstLine("%s: %s to launch", color.New(color.FgBlue, color.Bold).Sprint("Node Modules"), color.YellowString("Contributing"))
+		m.logger.FirstLine("%s: %s to launch", boldNode, color.YellowString("Contributing"))
 
 		if vendored {
-			m.logger.FirstLine("%s: %s", color.New(color.FgBlue, color.Bold).Sprint("Node Modules"), color.YellowString("Rebuilding"))
-			if err := m.npm.Rebuild(m.app.Root); err != nil {
+			m.logger.FirstLine("Removing cached node_modules")
+			if err := os.RemoveAll(cacheModulesDir); err != nil {
+				return fmt.Errorf("failed to remove cached node_modules: %v", err)
+			}
+			m.logger.FirstLine("%s: %s to cache", boldNode, color.YellowString("Copying"))
+			if err := m.copyModulesToLayer(appModulesDir, cacheModulesDir); err != nil {
+				return fmt.Errorf("failed to copy node_modules to the cache: %v", err)
+			}
+
+			m.logger.FirstLine("%s: %s", boldNode, color.YellowString("Rebuilding"))
+			if err := m.npm.Rebuild(m.cacheLayer.Root); err != nil {
 				return fmt.Errorf("failed to rebuild node_modules: %v", err)
 			}
 		} else {
-			m.logger.FirstLine("%s: %s", color.New(color.FgBlue, color.Bold).Sprint("Node Modules"), color.YellowString("Installing"))
-			if err := m.npm.Install(m.app.Root); err != nil {
+			m.logger.FirstLine("%s: %s to cache", color.New(color.FgBlue, color.Bold).Sprint("package.json"), color.YellowString("Copying"))
+
+			if err := os.MkdirAll(m.cacheLayer.Root, 0777); err != nil {
+				return fmt.Errorf("failed to create directory %s : %v", m.cacheLayer.Root, err)
+
+			}
+
+			appPackageJsonPath := filepath.Join(m.app.Root, "package.json")
+			cachePackageJsonPath := filepath.Join(m.cacheLayer.Root, "package.json")
+
+			if err := utils.CopyFile(appPackageJsonPath, cachePackageJsonPath); err != nil {
+				return fmt.Errorf("failed to copy package.json : %v", err)
+			}
+
+			appPackageLockPath := filepath.Join(m.app.Root, "package-lock.json")
+			cachePackageLockPath := filepath.Join(m.cacheLayer.Root, "package-lock.json")
+
+			if err := utils.CopyFile(appPackageLockPath, cachePackageLockPath); err != nil {
+				return fmt.Errorf("failed to copy package-lock.json: %v", err)
+			}
+
+			m.logger.FirstLine("%s: %s", boldNode, color.YellowString("Installing"))
+			if err := m.npm.Install(m.app.Root, m.cacheLayer.Root); err != nil {
 				return fmt.Errorf("failed to install node_modules: %v", err)
 			}
 		}
 
-		if err := m.copyModulesToLayer(launchModulesDir); err != nil {
+		if err := m.copyModulesToLayer(cacheModulesDir, launchModulesDir); err != nil {
 			return fmt.Errorf("failed to copy the node_modules to the launch layer: %v", err)
 		}
 
@@ -107,7 +147,7 @@ func (m Modules) Contribute() error {
 			return fmt.Errorf("failed to write metadata to package-lock.json: %v", err)
 		}
 	} else {
-		m.logger.FirstLine("%s: %s cached launch layer", color.New(color.FgBlue, color.Bold).Sprint("Node Modules"), color.GreenString("Reusing"))
+		m.logger.FirstLine("%s: %s cached launch layer", boldNode, color.GreenString("Reusing"))
 	}
 
 	m.logger.SubsequentLine("Cleaning up node_modules")
@@ -121,17 +161,6 @@ func (m Modules) Contribute() error {
 	}
 
 	return nil
-}
-
-func (m *Modules) CreateLaunchMetadata() libbuildpackV3.LaunchMetadata {
-	return libbuildpackV3.LaunchMetadata{
-		Processes: libbuildpackV3.Processes{
-			libbuildpackV3.Process{
-				Type:    "web",
-				Command: "npm start",
-			},
-		},
-	}
 }
 
 func (m Modules) packageLockMatchesMetadataSha() (bool, error) {
@@ -174,7 +203,7 @@ func (m Modules) writeMetadataSha(path string) error {
 	})
 }
 
-func (m *Modules) copyModulesToLayer(dest string) error {
+func (m *Modules) copyModulesToLayer(src, dest string) error {
 	if exist, err := libjavabuildpack.FileExists(dest); err != nil {
 		return err
 	} else if !exist {
@@ -183,7 +212,7 @@ func (m *Modules) copyModulesToLayer(dest string) error {
 		}
 	}
 
-	if err := utils.CopyDirectory(filepath.Join(m.app.Root, "node_modules"), dest); err != nil {
+	if err := utils.CopyDirectory(src, dest); err != nil {
 		return err
 	}
 
