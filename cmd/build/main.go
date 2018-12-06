c9c3224186b8efda5ebb4432bb7f67273151ba7a
Danny Joyce <djoyce@pivotal.io>
2018-12-06 17:42:38 -0500

    Rename builder and detector to context

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -11,33 +11,33 @@ import (
 )
 
 func main() {
-	builder, err := build.DefaultBuild()
+	context, err := build.DefaultBuild()
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "failed to create default builder: %s", err)
+		_, _ = fmt.Fprintf(os.Stderr, "failed to create default build context: %s", err)
 		os.Exit(100)
 	}
 
-	code, err := runBuild(builder)
+	code, err := runBuild(context)
 	if err != nil {
-		builder.Logger.Info(err.Error())
+		context.Logger.Info(err.Error())
 	}
 
 	os.Exit(code)
 }
 
-func runBuild(builder build.Build) (int, error) {
-	builder.Logger.FirstLine(builder.Logger.PrettyIdentity(builder.Buildpack))
+func runBuild(context build.Build) (int, error) {
+	context.Logger.FirstLine(context.Logger.PrettyIdentity(context.Buildpack))
 
-	contributor, willContribute, err := modules.NewContributor(builder, npm.NPM{})
+	contributor, willContribute, err := modules.NewContributor(context, npm.NPM{})
 	if err != nil {
-		return builder.Failure(102), err
+		return context.Failure(102), err
 	}
 
 	if willContribute {
 		if err := contributor.Contribute(); err != nil {
-			return builder.Failure(103), err
+			return context.Failure(103), err
 		}
 	}
 
-	return builder.Success(buildplan.BuildPlan{})
+	return context.Success(buildplan.BuildPlan{})
 }
--- cmd/detect/main.go
+++ cmd/detect/main.go
@@ -13,35 +13,35 @@ import (
 )
 
 func main() {
-	detector, err := detect.DefaultDetect()
+	context, err := detect.DefaultDetect()
 	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "failed to create default detector: %s", err)
+		_, _ = fmt.Fprintf(os.Stderr, "failed to create default detect context: %s", err)
 		os.Exit(100)
 	}
 
-	code, err := runDetect(detector)
+	code, err := runDetect(context)
 	if err != nil {
-		detector.Logger.Info(err.Error())
+		context.Logger.Info(err.Error())
 	}
 
 	os.Exit(code)
 }
 
-func runDetect(detector detect.Detect) (int, error) {
-	packageJSON := filepath.Join(detector.Application.Root, "package.json")
+func runDetect(context detect.Detect) (int, error) {
+	packageJSON := filepath.Join(context.Application.Root, "package.json")
 
 	if exists, err := layers.FileExists(packageJSON); err != nil {
-		return detector.Fail(), fmt.Errorf("error checking filepath: %s", packageJSON)
+		return context.Fail(), fmt.Errorf("error checking filepath: %s", packageJSON)
 	} else if !exists {
-		return detector.Fail(), fmt.Errorf(`no "package.json" found at: %s`, packageJSON)
+		return context.Fail(), fmt.Errorf(`no "package.json" found at: %s`, packageJSON)
 	}
 
 	version, err := node.GetNodeVersion(packageJSON)
 	if err != nil {
-		return detector.Fail(), fmt.Errorf(`unable to parse "package.json": %s`, err.Error())
+		return context.Fail(), fmt.Errorf(`unable to parse "package.json": %s`, err.Error())
 	}
 
-	return detector.Pass(buildplan.BuildPlan{
+	return context.Pass(buildplan.BuildPlan{
 		node.Dependency: buildplan.Dependency{
 			Version:  version,
 			Metadata: buildplan.Metadata{"build": true, "launch": true},
