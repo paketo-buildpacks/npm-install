f81ffbce51fb8a698862b763b8893cc3530a1b6e
Danny Joyce <djoyce@pivotal.io>
2019-02-04 16:45:32 -0500

    Fix install tools script to add cflinuxfs3 stack [#163595064]
    Co-authored-by: David Freilich <dfreilich@pivotal.io>

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -17,11 +17,16 @@ install_pack() {
 
     PACK_ARTIFACT=pack-$PACK_VERSION-$OS.tar.gz
 
-    wget https://github.com/buildpack/pack/releases/download/v$PACK_VERSION/$PACK_ARTIFACT
+    curl -O -L -s https://github.com/buildpack/pack/releases/download/v$PACK_VERSION/$PACK_ARTIFACT
     tar xzvf $PACK_ARTIFACT -C .bin
     rm $PACK_ARTIFACT
 }
 
+configure_pack() {
+    pack add-stack org.cloudfoundry.stacks.cflinuxfs3 \
+        --build-image cfbuildpacks/cflinuxfs3-cnb-experimental:build \
+        --run-image cfbuildpacks/cflinuxfs3-cnb-experimental:run || echo "Ignoring add stack error"
+}
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/.."
 
@@ -34,3 +39,4 @@ elif [[ $(.bin/pack version | cut -d ' ' -f 2) != "v$PACK_VERSION" ]]; then
     rm .bin/pack
     install_pack
 fi
+configure_pack
