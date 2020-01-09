301b36a59b50ca96193f81b7c158ad82af816025
Ryan Moran <rmoran@pivotal.io>
2020-01-09 11:59:56 -0500

    Removes test coverage for installing a specific version of npm
    [#170138082]
    
    Co-authored-by: Forest Eckhardt <feckhardt@pivotal.io>

--- integration/versioning_test.go
+++ integration/versioning_test.go
@@ -21,32 +21,6 @@ func testVersioning(t *testing.T, context spec.G, it spec.S) {
 		Expect(app.Destroy()).To(Succeed())
 	})
 
-	context("when the npm version minor patch is floated", func() {
-		// NOTE: this regression test was based on v2 buildpack functionality that
-		// installed a version of npm that was specified by the user in the
-		// package.json file. We seem to have skipped this functionality in the
-		// CNB. We are waiting on feedback for whether we should be implementing
-		// that behavior here and adding test coverage for it, or removing this as
-		// an unnecessary test.
-		it.Pend("builds a working OCI image, but not respect specified npm version", func() {
-			var err error
-			app, err = dagger.NewPack(
-				filepath.Join("testdata", "npm_version_with_minor_x"),
-				dagger.RandomImage(),
-				dagger.SetBuildpacks(nodeURI, npmURI),
-			).Build()
-			Expect(err).ToNot(HaveOccurred())
-
-			Expect(app.Start()).To(Succeed())
-
-			resp, err := app.HTTPGetBody("/")
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(resp).To(MatchRegexp(`Hello, World! From npm version: \d+\.\d+.\d+`))
-			Expect(resp).NotTo(MatchRegexp(`Hello, World! From npm version: 99.99.99`))
-		})
-	})
-
 	context("when using a nvmrc file", func() {
 		const nvmrcVersion = `8.\d+\.\d+`
 
