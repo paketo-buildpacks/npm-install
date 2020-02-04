b313b826ce1eb2aa5c3b9264db14ba5842651950
Forest Eckhardt <feckhardt@pivotal.io>
2020-02-04 09:55:16 -0500

    Move defer calls to after variable reassignment
    Co-authored-by: Ryan Moran <rmoran@pivotal.io>

--- integration/init_test.go
+++ integration/init_test.go
@@ -31,28 +31,29 @@ func TestIntegration(t *testing.T) {
 
 	npmURI, err = dagger.PackageBuildpack(bpDir)
 	Expect(err).ToNot(HaveOccurred())
-	defer dagger.DeleteBuildpack(npmURI)
 
 	npmCachedURI, _, err = dagger.PackageCachedBuildpack(bpDir)
 	Expect(err).ToNot(HaveOccurred())
-	defer dagger.DeleteBuildpack(npmCachedURI)
 
 	nodeURI, err = dagger.GetLatestBuildpack("node-engine-cnb")
 	Expect(err).ToNot(HaveOccurred())
-	defer dagger.DeleteBuildpack(nodeURI)
 
 	nodeRepo, err := dagger.GetLatestUnpackagedBuildpack("node-engine-cnb")
 	Expect(err).ToNot(HaveOccurred())
-	defer os.RemoveAll(nodeRepo)
 
 	nodeCachedURI, _, err = dagger.PackageCachedBuildpack(nodeRepo)
 	Expect(err).ToNot(HaveOccurred())
-	defer dagger.DeleteBuildpack(nodeCachedURI)
 
 	// HACK: we need to fix dagger and the package.sh scripts so that this isn't required
 	npmURI = fmt.Sprintf("%s.tgz", npmURI)
 	npmCachedURI = fmt.Sprintf("%s.tgz", npmCachedURI)
 	nodeCachedURI = fmt.Sprintf("%s.tgz", nodeCachedURI)
+
+	defer dagger.DeleteBuildpack(npmURI)
+	defer dagger.DeleteBuildpack(npmCachedURI)
+	defer dagger.DeleteBuildpack(nodeURI)
+	defer os.RemoveAll(nodeRepo)
+	defer dagger.DeleteBuildpack(nodeCachedURI)
 
 	suite := spec.New("Integration", spec.Parallel(), spec.Report(report.Terminal{}))
 	suite("Caching", testCaching)
