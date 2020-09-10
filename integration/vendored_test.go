2615e13679bd780428195db71adc59737746e941
Josh Zarrabi <jzarrabi@vmware.com>
2020-09-10 15:03:56 +0000

    add empty line to end of logging
    Co-authored-by: Arjun Sreedharan <asreedharan@vmware.com>

--- integration/vendored_test.go
+++ integration/vendored_test.go
@@ -104,6 +104,7 @@ func testVendored(t *testing.T, context spec.G, it spec.S) {
 				"    NPM_CONFIG_LOGLEVEL   -> \"error\"",
 				"    NPM_CONFIG_PRODUCTION -> \"true\"",
 				fmt.Sprintf("    PATH                  -> \"$PATH:/layers/%s/modules/node_modules/.bin\"", strings.ReplaceAll(buildpackInfo.Buildpack.ID, "/", "_")),
+				"",
 			))
 		})
 
