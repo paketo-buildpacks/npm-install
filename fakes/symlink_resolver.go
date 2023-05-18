04fa4005bbafa5922bfb9685c40c222e633dce9a
Tim Hitchener <thitch97@users.noreply.github.com>
2023-05-18 15:38:51 -0400

    Fix workspace resolution when modules required at build and launch; fix launch-time symlinking for workspace packages (#512)

--- fakes/symlink_resolver.go
+++ fakes/symlink_resolver.go
@@ -1,8 +1,37 @@
 package fakes
 
-import "sync"
+import (
+	"sync"
+
+	npminstall "github.com/paketo-buildpacks/npm-install"
+)
 
 type SymlinkResolver struct {
+	CopyCall struct {
+		mutex     sync.Mutex
+		CallCount int
+		Receives  struct {
+			LockfilePath    string
+			SourceLayerPath string
+			TargetLayerPath string
+		}
+		Returns struct {
+			Error error
+		}
+		Stub func(string, string, string) error
+	}
+	ParseLockfileCall struct {
+		mutex     sync.Mutex
+		CallCount int
+		Receives  struct {
+			LockfilePath string
+		}
+		Returns struct {
+			Lockfile npminstall.Lockfile
+			Error    error
+		}
+		Stub func(string) (npminstall.Lockfile, error)
+	}
 	ResolveCall struct {
 		mutex     sync.Mutex
 		CallCount int
@@ -17,6 +46,28 @@ type SymlinkResolver struct {
 	}
 }
 
+func (f *SymlinkResolver) Copy(param1 string, param2 string, param3 string) error {
+	f.CopyCall.mutex.Lock()
+	defer f.CopyCall.mutex.Unlock()
+	f.CopyCall.CallCount++
+	f.CopyCall.Receives.LockfilePath = param1
+	f.CopyCall.Receives.SourceLayerPath = param2
+	f.CopyCall.Receives.TargetLayerPath = param3
+	if f.CopyCall.Stub != nil {
+		return f.CopyCall.Stub(param1, param2, param3)
+	}
+	return f.CopyCall.Returns.Error
+}
+func (f *SymlinkResolver) ParseLockfile(param1 string) (npminstall.Lockfile, error) {
+	f.ParseLockfileCall.mutex.Lock()
+	defer f.ParseLockfileCall.mutex.Unlock()
+	f.ParseLockfileCall.CallCount++
+	f.ParseLockfileCall.Receives.LockfilePath = param1
+	if f.ParseLockfileCall.Stub != nil {
+		return f.ParseLockfileCall.Stub(param1)
+	}
+	return f.ParseLockfileCall.Returns.Lockfile, f.ParseLockfileCall.Returns.Error
+}
 func (f *SymlinkResolver) Resolve(param1 string, param2 string) error {
 	f.ResolveCall.mutex.Lock()
 	defer f.ResolveCall.mutex.Unlock()
