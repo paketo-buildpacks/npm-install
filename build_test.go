ebe887772f1c40eddf1576e65a8d4454fdd2740b
Ralf Pannemans <ralf.pannemans@sap.com>
2024-06-18 17:18:18 +0200

    Adapt unit test to expect prepend of .bin
    Co-authored-by: Ralf Pannemans <ralf.pannemans@sap.com>

--- build_test.go
+++ build_test.go
@@ -156,7 +156,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 			Expect(buildLayer.Path).To(Equal(filepath.Join(layersDir, "build-modules")))
 			Expect(buildLayer.SharedEnv).To(Equal(packit.Environment{}))
 			Expect(buildLayer.BuildEnv).To(Equal(packit.Environment{
-				"PATH.append":       filepath.Join(layersDir, "build-modules", "node_modules", ".bin"),
+				"PATH.prepend":      filepath.Join(layersDir, "build-modules", "node_modules", ".bin"),
 				"PATH.delim":        ":",
 				"NODE_ENV.override": "development",
 			}))
@@ -306,7 +306,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 			Expect(launchLayer.LaunchEnv).To(Equal(packit.Environment{
 				"NPM_CONFIG_LOGLEVEL.default": "error",
 				"NODE_PROJECT_PATH.default":   workingDir,
-				"PATH.append":                 filepath.Join(layersDir, "launch-modules", "node_modules", ".bin"),
+				"PATH.prepend":                filepath.Join(layersDir, "launch-modules", "node_modules", ".bin"),
 				"PATH.delim":                  ":",
 			}))
 			Expect(launchLayer.ProcessLaunchEnv).To(Equal(map[string]packit.Environment{}))
@@ -458,7 +458,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 			Expect(buildLayer.Path).To(Equal(filepath.Join(layersDir, "build-modules")))
 			Expect(buildLayer.SharedEnv).To(Equal(packit.Environment{}))
 			Expect(buildLayer.BuildEnv).To(Equal(packit.Environment{
-				"PATH.append":       filepath.Join(layersDir, "build-modules", "node_modules", ".bin"),
+				"PATH.prepend":      filepath.Join(layersDir, "build-modules", "node_modules", ".bin"),
 				"PATH.delim":        ":",
 				"NODE_ENV.override": "development",
 			}))
@@ -553,7 +553,7 @@ func testBuild(t *testing.T, context spec.G, it spec.S) {
 			Expect(launchLayer.LaunchEnv).To(Equal(packit.Environment{
 				"NPM_CONFIG_LOGLEVEL.default": "error",
 				"NODE_PROJECT_PATH.default":   workingDir,
-				"PATH.append":                 filepath.Join(layersDir, "launch-modules", "node_modules", ".bin"),
+				"PATH.prepend":                filepath.Join(layersDir, "launch-modules", "node_modules", ".bin"),
 				"PATH.delim":                  ":",
 			}))
 			Expect(launchLayer.ProcessLaunchEnv).To(Equal(map[string]packit.Environment{}))
